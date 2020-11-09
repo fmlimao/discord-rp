0253d64600d5734359f704b4846a395babf43955
Leandro Macedo <leandro.dmacedo@jsl.com.br>
2020-11-09 14:22:50 -0300

    Criando navegação do site

---
+++ site/public/js/site.js
@@ -0,0 +1,125 @@
+var data = {
+    pages: {
+        'planos-vips': {
+            items: [
+                { code: '3001', title: 'Plano 1', description: 'Descrição do plano 1', price: 'R$ 19,99/mês', image: '/assets/img/vips/bronze.png' },
+                { code: '3002', title: 'Plano 2', description: 'Descrição do plano 2', price: 'R$ 29,99/mês', image: '/assets/img/vips/bronze.png' },
+                { code: '3003', title: 'Plano 3', description: 'Descrição do plano 3', price: 'R$ 39,99/mês', image: '/assets/img/vips/bronze.png' },
+                { code: '3004', title: 'Plano 4', description: 'Descrição do plano 4', price: 'R$ 49,99/mês', image: '/assets/img/vips/bronze.png' },
+                { code: '3005', title: 'Plano 5', description: 'Descrição do plano 5', price: 'R$ 59,99/mês', image: '/assets/img/vips/bronze.png' },
+                { code: '3006', title: 'Plano 6', description: 'Descrição do plano 6', price: 'R$ 69,99/mês', image: '/assets/img/vips/bronze.png' },
+                { code: '3007', title: 'Plano 7', description: 'Descrição do plano 7', price: 'R$ 79,99/mês', image: '/assets/img/vips/bronze.png' },
+                { code: '3008', title: 'Plano 8', description: 'Descrição do plano 8', price: 'R$ 89,99/mês', image: '/assets/img/vips/bronze.png' },
+                { code: '3009', title: 'Plano 9', description: 'Descrição do plano 9', price: 'R$ 99,99/mês', image: '/assets/img/vips/bronze.png' },
+                { code: '3010', title: 'Plano 10', description: 'Descrição do plano 10', price: 'R$ 109,99/mês', image: '/assets/img/vips/bronze.png' },
+            ],
+        },
+        'veiculos-vips': {
+            items: [
+                { code: '4001', title: 'Veículo 1', description: 'Descrição do veículo 1', price: 'R$ 19,99/mês', image: '/assets/img/veiculos/pacote.png' },
+                { code: '4002', title: 'Veículo 2', description: 'Descrição do veículo 2', price: 'R$ 29,99/mês', image: '/assets/img/veiculos/pacote.png' },
+                { code: '4003', title: 'Veículo 3', description: 'Descrição do veículo 3', price: 'R$ 39,99/mês', image: '/assets/img/veiculos/pacote.png' },
+                { code: '4004', title: 'Veículo 4', description: 'Descrição do veículo 4', price: 'R$ 49,99/mês', image: '/assets/img/veiculos/pacote.png' },
+                { code: '4005', title: 'Veículo 5', description: 'Descrição do veículo 5', price: 'R$ 59,99/mês', image: '/assets/img/veiculos/pacote.png' },
+                { code: '4006', title: 'Veículo 6', description: 'Descrição do veículo 6', price: 'R$ 69,99/mês', image: '/assets/img/veiculos/pacote.png' },
+                { code: '4007', title: 'Veículo 7', description: 'Descrição do veículo 7', price: 'R$ 79,99/mês', image: '/assets/img/veiculos/pacote.png' },
+                { code: '4008', title: 'Veículo 8', description: 'Descrição do veículo 8', price: 'R$ 89,99/mês', image: '/assets/img/veiculos/pacote.png' },
+                { code: '4009', title: 'Veículo 9', description: 'Descrição do veículo 9', price: 'R$ 99,99/mês', image: '/assets/img/veiculos/pacote.png' },
+                { code: '4010', title: 'Veículo 10', description: 'Descrição do veículo 10', price: 'R$ 109,99/mês', image: '/assets/img/veiculos/pacote.png' },
+            ],
+        },
+        'dinheiro': {
+            items: [
+                { code: '5001', title: 'Dinheiro 1', description: 'Descrição do dinheiro 1', price: 'R$ 19,99/mês', image: '/assets/img/dinheiro/cartao.png' },
+                { code: '5002', title: 'Dinheiro 2', description: 'Descrição do dinheiro 2', price: 'R$ 29,99/mês', image: '/assets/img/dinheiro/cartao.png' },
+                { code: '5003', title: 'Dinheiro 3', description: 'Descrição do dinheiro 3', price: 'R$ 39,99/mês', image: '/assets/img/dinheiro/cartao.png' },
+                { code: '5004', title: 'Dinheiro 4', description: 'Descrição do dinheiro 4', price: 'R$ 49,99/mês', image: '/assets/img/dinheiro/cartao.png' },
+                { code: '5005', title: 'Dinheiro 5', description: 'Descrição do dinheiro 5', price: 'R$ 59,99/mês', image: '/assets/img/dinheiro/cartao.png' },
+                { code: '5006', title: 'Dinheiro 6', description: 'Descrição do dinheiro 6', price: 'R$ 69,99/mês', image: '/assets/img/dinheiro/cartao.png' },
+                { code: '5007', title: 'Dinheiro 7', description: 'Descrição do dinheiro 7', price: 'R$ 79,99/mês', image: '/assets/img/dinheiro/cartao.png' },
+                { code: '5008', title: 'Dinheiro 8', description: 'Descrição do dinheiro 8', price: 'R$ 89,99/mês', image: '/assets/img/dinheiro/cartao.png' },
+                { code: '5009', title: 'Dinheiro 9', description: 'Descrição do dinheiro 9', price: 'R$ 99,99/mês', image: '/assets/img/dinheiro/cartao.png' },
+                { code: '5010', title: 'Dinheiro 10', description: 'Descrição do dinheiro 10', price: 'R$ 109,99/mês', image: '/assets/img/dinheiro/cartao.png' },
+            ],
+        },
+        'armas': {
+            items: [
+                { code: '6001', title: 'Arma 1', description: 'Descrição do arma 1', price: 'R$ 19,99/mês', image: '/assets/img/armas/pacote.png' },
+                { code: '6002', title: 'Arma 2', description: 'Descrição do arma 2', price: 'R$ 29,99/mês', image: '/assets/img/armas/pacote.png' },
+                { code: '6003', title: 'Arma 3', description: 'Descrição do arma 3', price: 'R$ 39,99/mês', image: '/assets/img/armas/pacote.png' },
+                { code: '6004', title: 'Arma 4', description: 'Descrição do arma 4', price: 'R$ 49,99/mês', image: '/assets/img/armas/pacote.png' },
+                { code: '6005', title: 'Arma 5', description: 'Descrição do arma 5', price: 'R$ 59,99/mês', image: '/assets/img/armas/pacote.png' },
+                { code: '6006', title: 'Arma 6', description: 'Descrição do arma 6', price: 'R$ 69,99/mês', image: '/assets/img/armas/pacote.png' },
+                { code: '6007', title: 'Arma 7', description: 'Descrição do arma 7', price: 'R$ 79,99/mês', image: '/assets/img/armas/pacote.png' },
+                { code: '6008', title: 'Arma 8', description: 'Descrição do arma 8', price: 'R$ 89,99/mês', image: '/assets/img/armas/pacote.png' },
+                { code: '6009', title: 'Arma 9', description: 'Descrição do arma 9', price: 'R$ 99,99/mês', image: '/assets/img/armas/pacote.png' },
+                { code: '6010', title: 'Arma 10', description: 'Descrição do arma 10', price: 'R$ 109,99/mês', image: '/assets/img/armas/pacote.png' },
+            ],
+        },
+        'remover-punicoes': {
+            items: [
+                { code: '7001', title: 'Item 1', description: 'Descrição do item 1', price: 'R$ 19,99/mês', image: '/assets/img/punicoes/1adv.png' },
+                { code: '7002', title: 'Item 2', description: 'Descrição do item 2', price: 'R$ 29,99/mês', image: '/assets/img/punicoes/1adv.png' },
+                { code: '7003', title: 'Item 3', description: 'Descrição do item 3', price: 'R$ 39,99/mês', image: '/assets/img/punicoes/1adv.png' },
+                { code: '7004', title: 'Item 4', description: 'Descrição do item 4', price: 'R$ 49,99/mês', image: '/assets/img/punicoes/1adv.png' },
+                { code: '7005', title: 'Item 5', description: 'Descrição do item 5', price: 'R$ 59,99/mês', image: '/assets/img/punicoes/1adv.png' },
+                { code: '7006', title: 'Item 6', description: 'Descrição do item 6', price: 'R$ 69,99/mês', image: '/assets/img/punicoes/1adv.png' },
+                { code: '7007', title: 'Item 7', description: 'Descrição do item 7', price: 'R$ 79,99/mês', image: '/assets/img/punicoes/1adv.png' },
+                { code: '7008', title: 'Item 8', description: 'Descrição do item 8', price: 'R$ 89,99/mês', image: '/assets/img/punicoes/1adv.png' },
+                { code: '7009', title: 'Item 9', description: 'Descrição do item 9', price: 'R$ 99,99/mês', image: '/assets/img/punicoes/1adv.png' },
+                { code: '7010', title: 'Item 10', description: 'Descrição do item 10', price: 'R$ 109,99/mês', image: '/assets/img/punicoes/1adv.png' },
+            ],
+        },
+    },
+};
+
+$(function () {
+
+    for (var page_id in data.pages) {
+        var page = data.pages[page_id];
+        var items = page.items;
+
+        for (var item_id in items) {
+            var item = items[item_id];
+
+            var html = `
+            <div class="col-xs-12 col-sm-4 col-md-4 col-lg-3">
+                <div class="panel panel-list">
+                    <div class="panel-body">
+                        <img src="${item.image}" class="img-responsive">
+
+                        <p class="text-center">
+                            ${item.title}
+                        </p>
+                    </div>
+
+                    <div class="panel-footer">
+                        <div class="row">
+                            <div class="col-xs-6">
+                                <button class="btn btn-dark-3 btn-block">Detalhes</button>
+                            </div>
+                            <div class="col-xs-6">
+                                <button class="btn btn-purple btn-block">Eu quero</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+
+            $('.page[data-page=' + page_id + '] > .row').append(html);
+        }
+    }
+
+    $('.btn-page').off('click').on('click', function (e) {
+        e.preventDefault();
+
+        var page = $(this).attr('data-page');
+
+        $('.btn-page').parent().removeClass('active');
+        $('.btn-page[data-page=' + page + ']').parent().addClass('active');
+
+        $('.page').removeClass('active');
+        $('.page[data-page=' + page + ']').addClass('active');
+    });
+
+});
